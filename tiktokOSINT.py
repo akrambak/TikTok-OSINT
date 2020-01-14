49f2cb33e81f19baf00a889de8c902daea1c8bfd
Stephen <stephen@Stephens-MacBook-Pro-2.local>
2020-01-14 13:17:37 -0600

    Update tiktokOSINT.py

--- tiktokOSINT.py
+++ tiktokOSINT.py
@@ -46,12 +46,18 @@ class TikTokOSINT:
 		soup = BeautifulSoup(r.text,'html.parser')
 		attrs = soup.find_all('meta')
 		meta1 = soup.find_all('script')
+		attrs = soup.find_all('meta')
 		try:
+			# content1 = attrs[27].get('content').split(',')
+			# We know that it is 13 we are looking for... Now we need to figure out why it won't load
+			#print(meta1[12].get_text())
+			# Need to clean up what is going on with the splicing of content -- figured out hte main issue
 			content1 = attrs[4].get('content').split('.')
-			meta1 = json.loads(meta1[8].get_text()[24:])
+			meta1 = json.loads(meta1[13].get_text())
+			print(meta1)
 			profiledata = {"username":self.username,
-			'followers':content1[4],
-			'following':content1[5],
+			'followers':content1[0],
+			'following':content1[1],
 			'bio':content1[6],
 			'profilepictureurl':attrs[13].get('content'),
 			'isVerified':meta1['/@:uniqueId']['userData']['verified'],
@@ -113,7 +119,6 @@ class TikTokOSINT:
 			print(f"{key.upper()}: {value}")
 
 
-
 def arg_parse():
 	parser = argparse.ArgumentParser(description="TikTok OSINT Tool")
 	parser.add_argument("--username", help="Profile Username", required=True, nargs=1)
@@ -132,4 +137,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
